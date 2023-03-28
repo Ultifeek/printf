7c723b93567009efe830e8c18c2c11d9e3c59eb8
Ultifeek <ultifeek1@gmail.com>
2023-03-28 12:51:43 +0000

    new _printf function commit

--- _printf.c
+++ _printf.c
@@ -1,51 +1,52 @@
-#include <stdarg.h>
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _printf - function that prints a formatted string
- *
  * @format: string to print (char *)
  * @...: variadic list of arguments
- *
  * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0, value = 0;
+	int i = 0, count = 0, value = 0, (*f)(va_list);
 
 	va_list args;
 
 	va_start(args, format);
 
-	int (*f)(va_list);
-
 	if (format == NULL)
 		return (-1);
-
 	while (format[i])
 	{
 		if (format[i] != '%')
+		{
 			value = write(1, &format[i], 1);
 			count = count + value;
 			i++;
 			continue;
+		}
 		if (format[i] == '%')
+		{
 			f = check_specifier(&format[i + 1]);
 
 			if (f != NULL)
+			{
 				value = f(args);
 				count = count + value;
 				i = i + 2;
 				continue;
+			}
 			if (format[i + 1] == '\0')
 				break;
 			if (format[i + 1] != '\0')
+			{
 				value = write(1, &format[i + 1], 1);
 				count = count + value;
 				i = i + 2;
 				continue;
+			}
+		}
 	}
 	return (count);
 }
