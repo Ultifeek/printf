d974bbce33c3867f67d1014a23add5b5bb0d884d
O-Kiks <samuel.o.raji@gmail.com>
2023-03-28 04:30:16 -0700

    function that prints formatted text to the standard output

--- _printf.c
+++ _printf.c
@@ -13,9 +13,8 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
+	int i = 0, count = 0, value = 0;
+
 	va_list args;
 
 	va_start(args, format);
@@ -23,43 +22,30 @@ int _printf(const char *format, ...)
 	int (*f)(va_list);
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 
 	while (format[i])
 	{
 		if (format[i] != '%')
-		{
 			value = write(1, &format[i], 1);
 			count = count + value;
 			i++;
 			continue;
-		}
-
 		if (format[i] == '%')
-		{
 			f = check_specifier(&format[i + 1]);
 
 			if (f != NULL)
-			{
 				value = f(args);
 				count = count + value;
 				i = i + 2;
 				continue;
-			}
 			if (format[i + 1] == '\0')
-			{
 				break;
-			}
 			if (format[i + 1] != '\0')
-			{
 				value = write(1, &format[i + 1], 1);
 				count = count + value;
 				i = i + 2;
 				continue;
-			}
-		}
 	}
 	return (count);
 }
